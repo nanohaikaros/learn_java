16e40c8c1d38878e7185ea7e9d5c407b6dda046d
Nanoha <1371689491@qq.com>
2019-08-07 21:11:53 +0800

    1

---
+++ src/Introduction/ComputeAreaWithConstant.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConstant {
+    public static void main(String[] args) {
+        final double PI = 3.14159;  // declare a constant
+
+        // create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // prompt the user to enter a radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
+        // compute area
+        double area = radius * radius * PI;
+
+        // display result
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
---
+++ src/Introduction/Welcome.java
@@ -0,0 +1,8 @@
+package Introduction;
+
+public class Welcome {
+    public static void main(String[] args) {
+        // display message welcome to java! on the console
+        System.out.println("Welcome to Java!");
+    }
+}
